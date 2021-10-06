73f8e56ee7bbad970af77f0879712fd2cb237163
Modyzone <mody.zone@outlook.com>
2021-10-06 17:47:44 +0200

    add new files

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
 morgan = require('morgan');
+// import bodyPraser here
+const bodyParser = require('body-parser'),
+methodOverride = require('method-override');
+// set bodyparser
+app.use(bodyParser.urlencoded({ extended: true}))
+// use bodyparser json to serialise data
+app.use(bodyParser.json())
+//use methodOverrid
+app.use(methodOverride())
+//use morgan and static
+app.use(morgan('common'))
+app.use(express.static('public'));
 
 let topMovies = [
 {
@@ -49,19 +61,19 @@ let topMovies = [
  app.get('/', (req, res)=> {
    res.send('Welcome to my Movies club!');
 });
-
 app.get('/documentation' , (req, res) => {
   res.sendFile('public/documentation.html',  { root: __dirname});
 });
-app.get('/topMovies', (req, res) => {
+app.get('/movies', (req, res) => {
   res.json(topMovies);
-}); 
+});
+app.get('/name/title', (req, res) => {
+  res.send('The Best 10 Movies!');
+});
 // listen for requests
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
   });
-
-const bodyParser = require('body-parser');
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
