f6368364876d985a0714433e73a0117737f3881f
Modyzone <mody.zone@outlook.com>
2021-10-06 21:16:49 +0200

    add new files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 morgan = require('morgan');
+uuid = require('uuid');
 // import bodyPraser here
 const bodyParser = require('body-parser'),
 methodOverride = require('method-override');
@@ -69,6 +70,44 @@ app.get('/movies', (req, res) => {
 });
 app.get('/name/title', (req, res) => {
   res.send('The Best 10 Movies!');
+});
+app.get('/director', (req, res) => {
+  res.send('Quentin Tarantino');
+});
+// Allow new users to register.
+app.post('/users', (req, res) => {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+// Update the "users" to update the user info (username)
+app.put('/users/:name/', (req, res) => {
+  let users = users.find((user) => { return user.name === req.params.name });
+
+  if (users) {
+    users.name[req.params.name] = parseInt(req.params.name);
+    res.status(201).send('User ' + req.params.name + ' was assigned a user of ' + req.params.name );
+  } else {
+    res.status(404).send('User with the name ' + req.params.name + ' was not found.');
+  }
+});
+
+// Allow users to remove a movie from their list of favorites 
+//(showing only a text that a movie has been removed—more on this later)
+app.delete('/users/:id', (req, res) => {
+  let user = user.find((user) => { return user.id === req.params.id });
+
+  if (user) {
+    user = user.filter((obj) => { return obj.id !== req.params.id });
+    res.status(201).send('User ' + req.params.id + ' was deleted.');
+  }
 });
 // listen for requests
 app.listen(8080, () => {
