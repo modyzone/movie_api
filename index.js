15f2b292f816fa15a539d3efec8db03df8214a54
Modyzone <mody.zone@outlook.com>
2021-10-27 20:14:22 +0200

    add new files

--- index.js
+++ index.js
@@ -116,57 +116,33 @@ showTimes = () => {
   return result;
 };
 // Allow new users to register.
-app.post('/users',
-    [ //validator entries
-        check('Username', 'Username is required, must be at least 5 characters').isLength({min: 5}), 
-        check('Username', 'Username contains non-alphanumeric characters - not allowed.').isAlphanumeric(),
-        check('Password', 'Password is required').not().isEmpty(),
-        check('Email', 'Email is invalid').isEmail()
-    ], (req, res) => {
-    //check validation
-    let errors = validationResult(req);
-    
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
-    }
-
-    let hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({Username: req.body.Username})
+app.post('/users', (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
     .then((user) => {
-        if (user) {
-            return res.status(400).send(req.body.Username + ' is already registered');
-        } else {
-            Users
-            .create({
-                Username: req.body.Username,
-                Password: hashedPassword,
-                Email: req.body.Email,
-                Birthdate: req.body.Birthdate
-            })
-            .then((user) => {res.status(201).json(user)})
-            .catch((error) => {
-                console.error(error);
-                res.status(500).send('Error: ' + error);
-            })
-        }
+      if (user) {
+        // If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        })
+        .then((user) => {
+          res.status(201).json(user);
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        });
+      }
     })
     .catch((error) => {
-        console.error(error);
-        res.status(500).send('Error: ' + error);
+      console.error(error);
+      res.status(500).send('Error: ' + error);
     });
-});
-  
-  // Get all users
-  app.get('/users', passport.authenticate('jwt', { session: false }), (req, res) => {
-    Users.find()
-      .then((users) => {
-          console.log("====users" , users)
-        res.status(201).json(users);
-      })
-      .catch((err) => {
-        console.error(err)
-        res.status(500).send('Error: ' + err);
-      });
   });
   // Get a user by username
   app.get('/users/:Username', (req, res) => {
