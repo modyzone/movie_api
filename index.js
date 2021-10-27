dc02b6b57252309df787011ead7b011e6756d79e
Modyzone <mody.zone@outlook.com>
2021-10-27 18:18:49 +0200

    add new files

--- index.js
+++ index.js
@@ -14,28 +14,8 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
-const path = require('path');
-const PORT = process.env.PORT || 5000;
-express()
-.use(express.static(path.join(__dirname, 'public')))
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'ejs')
-.get('/', (req, res) => res.render('pages/index'))
-.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query('SELECT * FROM test_table');
-    const results = { 'result': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send('Error ' + err);
-  }
-})
-.get('/cool', (req, res) => res.send(cool()))
-.listen(PORT, () => console.log(`Listening on ${ PORT }`));
-const { check, validationResult } = require('express-validator');
+
+
 
 const Movies = Models.Movie;
 const Users = Models.User;
