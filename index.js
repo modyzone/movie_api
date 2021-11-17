7881ff057450474c4c18e38a7f3fd3d8dab5e197
Modyzone <mody.zone@outlook.com>
2021-11-17 17:45:53 +0100

    add new files

--- index.js
+++ index.js
@@ -202,6 +202,27 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (r
       res.status(500).send('Error: ' + err);
     });
 });
+/* POST login. */
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user: user
+        });
+      }
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        let token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
+  });
+}
+
 // Get a user by username
 /* app.get('/users/:Username', (req, res) => {
    Users.findOne({ Username: req.params.Username })
